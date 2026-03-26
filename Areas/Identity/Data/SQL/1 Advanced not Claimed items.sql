6bb2ecbf35ffafba3b4999c8c2ea298a3cb85440
Niket Saini <ac150381@avcol.school.nz>
2026-03-27 10:10:37 +1300

    added 1 complex and now i have total 6 simple queries 1 advanced and 1 complex

---
+++ Areas/Identity/Data/SQL/1 Advanced not Claimed items.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+    LI.ItemName, 
+    LI.Description, 
+    LI.DateFound, 
+    L.LocationName, 
+    C.Name AS Category,
+    CASE 
+        WHEN LI.IsClaimed = 1 THEN 'Claimed' 
+        ELSE 'Not Claimed' 
+    END AS IsClaimedStatus
+FROM LostItem AS LI
+JOIN Location AS L ON LI.LocationID = L.LocationID
+JOIN Category AS C ON LI.CategoryID = C.CategoryID
+WHERE LI.IsClaimed = 0;
