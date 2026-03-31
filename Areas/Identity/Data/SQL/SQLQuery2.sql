02d5c40806ad5673981cc7a52d79f23fbb8b2f09
Niket Saini <ac150381@avcol.school.nz>
2026-04-01 12:32:11 +1300

    fixed create

---
+++ Areas/Identity/Data/SQL/SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿-- Delete child table first
+DELETE FROM Claim;
+
+-- Then delete dependent LostItems
+DELETE FROM LostItem;
+
+-- Then delete parent tables
+DELETE FROM Category;
+DELETE FROM Location;
+
+-- Optionally, reset identity columns to start from 1
+DBCC CHECKIDENT ('Claim', RESEED, 0);
+DBCC CHECKIDENT ('LostItem', RESEED, 0);
+DBCC CHECKIDENT ('Category', RESEED, 0);
+DBCC CHECKIDENT ('Location', RESEED, 0);
